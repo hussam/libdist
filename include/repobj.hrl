fd93017f9f5a424c128071e6bbe04af850184d47
Hussam Abu-Libdeh <hussam@0xff.co>
2012-05-27 12:20:27 -0400

    Changed entry order in #conf record

--- include/repobj.hrl
+++ include/repobj.hrl
@@ -1,6 +1,6 @@
 -record(conf, {
-      protocol,      % replication protocol to use
-      args,          % optional arguments to be used at protocol's discretion
       version = 0,   % configuration version number
-      pids = []      % list of replicas in the configuration
+      pids = [],     % list of replicas in the configuration
+      protocol,      % replication protocol to use
+      args           % optional arguments to be used at protocol's discretion
    }).
