19164587e71e051d886294e5bfb9755854424e8b
Hussam Abu-Libdeh <hussam@0xff.co>
2013-02-22 15:18:20 -0500

    Changed the singleton module so that it doesn't have to conform to repobj's interface since it is not a replicated object

--- src/singleton.erl
+++ src/singleton.erl
@@ -1,10 +1,8 @@
 -module(singleton).
 -export([
-      new/4,
-      new_replica/3,
+      new/3,
       do/3,
-      reconfigure/4,
-      stop/4
+      stop/3
    ]).
 
 % Server callbacks
@@ -17,28 +15,13 @@
 
 -define(AllowSideEffects, true).
 
-new(CoreSettings, _Args, [Node], _Retry) ->
-   Replica = [ new_replica(Node, CoreSettings, _Args) ],
-   #rconf{protocol = ?MODULE, version = 1, pids = Replica}.  % return config
+new(Node, CoreModule, CoreArgs) ->
+   server:start(Node, ?MODULE, {CoreModule, CoreArgs}).
 
-new_replica(Node, CoreSettings, _RepArgs) ->
-   server:start(Node, ?MODULE, CoreSettings).
-
-do(_Obj=#rconf{pids=[Pid]}, Command, Retry) ->
+do(Pid, Command, Retry) ->
    libdist_utils:call(Pid, command, Command, Retry).
 
-reconfigure(OldConf, NewReplica, _NewArgs, Retry) ->
-   #rconf{version = Vn, pids = [OldReplica]} = OldConf,
-   NewConf = OldConf#rconf{version = Vn + 1, pids = [NewReplica]},
-   libdist_utils:call(OldReplica, reconfigure, NewConf, Retry),
-   if
-      NewReplica == OldReplica -> do_nothing;
-      true -> libdist_utils:call(NewReplica, reconfigure, NewConf, Retry)
-   end,
-   NewConf.
-
-stop(Obj, N, Reason, Retry) ->
-   Pid = lists:nth(N, Obj#rconf.pids),
+stop(Pid, Reason, Retry) ->
    libdist_utils:call(Pid, stop, Reason, Retry).
 
 
@@ -48,27 +31,20 @@ stop(Obj, N, Reason, Retry) ->
 
 % Initialize the state of the new replica
 init(_Me, {CoreModule, CoreArgs}) ->
-   Core = libdist_sm:new(CoreModule, CoreArgs),
-   Conf = #rconf{protocol = ?MODULE, version = 1, pids = [self()]},
-   {Core, Conf}.
+   libdist_sm:new(CoreModule, CoreArgs).
 
 
 % Handle a queued message a standalone replica (i.e. no replication)
-handle_msg(_Me, Message, {Core, Conf}) ->
+handle_msg(Me, Message, Core) ->
    case Message of
       % Handle a command for the core
       {Ref, Client, command, Command} ->
          Client ! {Ref, Core:do(?AllowSideEffects, Command)},
          consume;
 
-      % Reconfigure this replica. This is meaningless here.
-      {Ref, Client, reconfigure, _} ->
-         Client ! {Ref, ok},
-         {consume, {Core, Conf#rconf{version = Conf#rconf.version + 1}}};
-
       % Return the current configuration
       {Ref, Client, get_conf} ->
-         Client ! {Ref, Conf},
+         Client ! {Ref, #rconf{protocol = ?MODULE, pids = [Me]}},
          consume;
 
       % Stop this replica
