627bc0aecb3a484138430ca0cb5e6a62219ec44f
Hussam Abu-Libdeh <hussam@0xff.co>
2013-07-13 15:34:36 -0400

    Fixed repobj:new/5 to allow creating new sharded objects without inheritance correctly. Also, fixed incorrect variable name from previous commit.

--- src/repobj.erl
+++ src/repobj.erl
@@ -15,25 +15,32 @@
 
 % Create a new replicated state machine
 new(PrtclMod, PrtclArgs, SMSettings={SMModule, _} , Nodes, Timeout) ->
-   % spawn new replicas
-   Replicas = [ replica:new(PrtclMod, PrtclArgs, SMSettings, N) || N <- Nodes ],
+   % spawn new processes
+   Members = case PrtclMod:type() of
+      ?REPL ->
+         [ replica:new(PrtclMod, PrtclArgs, SMSettings, N) || N <- Nodes ];
+      ?PART ->
+         [ {T, replica:new(PrtclMod, PrtclArgs, SMSettings, N)} || {T,N} <- Nodes ]
+   end,
    % configure replicas and return configuration
-   configure(PrtclMod, PrtclArgs, SMModule, Replicas, ?NoSA, Timeout).
+   configure(PrtclMod, PrtclArgs, SMModule, Members, ?NoSA, Timeout).
 
 
 % Create a new replicated state machine and prepare it to later inherit the
 % state of an existing process
 inherit(OldPid, PrtclMod, PrtclArgs, Nodes, Timeout) ->
    % spawn new processes
-   Members = case PModule:type() of
-      ?REPL -> [ replica:new(PrtclMod, PrtclArgs, no_sm, N) || N <- Nodes ];
-      ?PART -> [ {T, replica:new(PrtclMod, PrtclArgs, no_sm, N)} || {T,N} <- Nodes ]
+   Members = case PrtclMod:type() of
+      ?REPL ->
+         [ replica:new(PrtclMod, PrtclArgs, no_sm, N) || N <- Nodes ];
+      ?PART ->
+         [ {T, replica:new(PrtclMod, PrtclArgs, no_sm, N)} || {T,N} <- Nodes ]
    end,
    % get the old process's state machine module
    {SMModule, OldConfType} = libdist_utils:call(OldPid,
       get_sm_module_and_conf_type, Timeout),
    % create a configuration and inform all the replicas of it and return it
-   ShardAgent = case (PModule:type() == ?PART) and (OldConfType /= ?SINGLE) of
+   ShardAgent = case (PrtclMod:type() == ?PART) and (OldConfType /= ?SINGLE) of
       true -> OldPid;
       false -> ?NoSA
    end,
@@ -42,7 +49,7 @@ inherit(OldPid, PrtclMod, PrtclArgs, Nodes, Timeout) ->
    % of the old one. This also results in replacing OldPid in each process's
    % local RP-Tree with Conf
    % TODO: handle timeouts properly
-   case PModule:type() of
+   case PrtclMod:type() of
       ?REPL ->
          libdist_utils:multicall(Members,
             {inherit_sm, OldPid, replicate, Timeout}, Timeout);
